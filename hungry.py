7ec411b4cc76fc4c6671be23f7c05852b4dc5c64
Surafel Argaw <sargaw3@gatech.edu>
2018-04-13 22:35:05 -0400

    this is thirsty

---
+++ hungry.py
@@ -0,0 +1,10 @@
+hungry = input("Are you hungry?")
+if hungry=="Yes":
+    print("You can have a sandwitch")
+else:
+    thirsty = input ("are you thirsty?")
+    if thirsty== "yes":
+        print("drnik some water")
+        print('I want to go home')
+    else:
+        print("sit and study")
