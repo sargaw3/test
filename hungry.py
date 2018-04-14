73d21e3ee3b5194134a58342bb384cab88446779
Surafel Argaw <sargaw3@gatech.edu>
2018-04-13 21:54:54 -0400

    sandwitch

--- hungry.py
+++ hungry.py
@@ -2,5 +2,6 @@ hungry = input("Are you hungry?")
 if hungry=="Yes"
     print("You can have a sandwitch")
     print("Eat pizza")
+    print ("Eat Sandwich")
 else
     print("wait until you are hungry")
