5c18a43eeea8843d1b36699153d8fc6ef734ee54
Surafel Argaw <sargaw3@gatech.edu>
2018-04-13 21:56:37 -0400

    Revert "sandwitch"
    This reverts commit 73d21e3ee3b5194134a58342bb384cab88446779.

--- hungry.py
+++ hungry.py
@@ -2,6 +2,5 @@ hungry = input("Are you hungry?")
 if hungry=="Yes"
     print("You can have a sandwitch")
     print("Eat pizza")
-    print ("Eat Sandwich")
 else
     print("wait until you are hungry")
